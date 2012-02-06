58040322eb57f6dc509a4addb61195dabb2b3602
nicecapj@gmail.com <nicecapj@gmail.com@a80e622d-c2b0-8f4f-c010-7ecd6fada85d>
2012-02-06 23:27:14 +0000

    mod:add rendermode option

--- qd3d9view_frontend/MapEditorD3D/GeneratedFiles/ui_mapeditord3d.h
+++ qd3d9view_frontend/MapEditorD3D/GeneratedFiles/ui_mapeditord3d.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mapeditord3d.ui'
 **
-** Created: Tue Feb 7 07:41:05 2012
+** Created: Tue Feb 7 08:25:58 2012
 **      by: Qt User Interface Compiler version 4.7.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
@@ -16,6 +16,7 @@
 #include <QtGui/QButtonGroup>
 #include <QtGui/QHeaderView>
 #include <QtGui/QMainWindow>
+#include <QtGui/QMenu>
 #include <QtGui/QMenuBar>
 #include <QtGui/QStatusBar>
 #include <QtGui/QToolBar>
@@ -26,9 +27,13 @@ QT_BEGIN_NAMESPACE
 class Ui_MapEditorD3DClass
 {
 public:
-    QMenuBar *menuBar;
-    QToolBar *mainToolBar;
+    QAction *actionSolid;
+    QAction *actionWire;
     QWidget *centralWidget;
+    QMenuBar *menuBar;
+    QMenu *menuRender;
+    QMenu *menuRenderMode;
+    QToolBar *mainToolBar;
     QStatusBar *statusBar;
 
     void setupUi(QMainWindow *MapEditorD3DClass)
@@ -36,18 +41,32 @@ public:
         if (MapEditorD3DClass->objectName().isEmpty())
             MapEditorD3DClass->setObjectName(QString::fromUtf8("MapEditorD3DClass"));
         MapEditorD3DClass->resize(600, 400);
-        menuBar = new QMenuBar(MapEditorD3DClass);
-        menuBar->setObjectName(QString::fromUtf8("menuBar"));
-        MapEditorD3DClass->setMenuBar(menuBar);
-        mainToolBar = new QToolBar(MapEditorD3DClass);
-        mainToolBar->setObjectName(QString::fromUtf8("mainToolBar"));
-        MapEditorD3DClass->addToolBar(mainToolBar);
+        actionSolid = new QAction(MapEditorD3DClass);
+        actionSolid->setObjectName(QString::fromUtf8("actionSolid"));
+        actionWire = new QAction(MapEditorD3DClass);
+        actionWire->setObjectName(QString::fromUtf8("actionWire"));
         centralWidget = new QWidget(MapEditorD3DClass);
         centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
         MapEditorD3DClass->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MapEditorD3DClass);
+        menuBar->setObjectName(QString::fromUtf8("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 600, 17));
+        menuRender = new QMenu(menuBar);
+        menuRender->setObjectName(QString::fromUtf8("menuRender"));
+        menuRenderMode = new QMenu(menuRender);
+        menuRenderMode->setObjectName(QString::fromUtf8("menuRenderMode"));
+        MapEditorD3DClass->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MapEditorD3DClass);
+        mainToolBar->setObjectName(QString::fromUtf8("mainToolBar"));
+        MapEditorD3DClass->addToolBar(Qt::TopToolBarArea, mainToolBar);
         statusBar = new QStatusBar(MapEditorD3DClass);
         statusBar->setObjectName(QString::fromUtf8("statusBar"));
         MapEditorD3DClass->setStatusBar(statusBar);
+
+        menuBar->addAction(menuRender->menuAction());
+        menuRender->addAction(menuRenderMode->menuAction());
+        menuRenderMode->addAction(actionSolid);
+        menuRenderMode->addAction(actionWire);
 
         retranslateUi(MapEditorD3DClass);
 
@@ -57,6 +76,10 @@ public:
     void retranslateUi(QMainWindow *MapEditorD3DClass)
     {
         MapEditorD3DClass->setWindowTitle(QApplication::translate("MapEditorD3DClass", "MapEditorD3D", 0, QApplication::UnicodeUTF8));
+        actionSolid->setText(QApplication::translate("MapEditorD3DClass", "Solid", 0, QApplication::UnicodeUTF8));
+        actionWire->setText(QApplication::translate("MapEditorD3DClass", "Wire", 0, QApplication::UnicodeUTF8));
+        menuRender->setTitle(QApplication::translate("MapEditorD3DClass", "Render", 0, QApplication::UnicodeUTF8));
+        menuRenderMode->setTitle(QApplication::translate("MapEditorD3DClass", "Render Mode", 0, QApplication::UnicodeUTF8));
     } // retranslateUi
 
 };
